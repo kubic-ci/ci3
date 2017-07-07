403c9b7de4681f66b127ecae9d8a75888fc41422
Yauhen Yakimovich <yauhen.yakimovich@pricehubble.com>
2017-07-08 00:24:05 +0200

    Adding some properties

--- ci3/commands/dotci3.py
+++ ci3/commands/dotci3.py
@@ -2,6 +2,7 @@
 import os
 from ci3.error import Ci3Error
 from .base import CliCommand
+import yaml
 
 
 class MissingDotCi3Folder(Ci3Error):
@@ -28,6 +29,23 @@ class DotCi3Mixin(object):
     def secrets_path(self):
         """Return fullpath string to `.ci3/secrets` folder."""
         return os.path.join(self.dotci3_path, 'secrets')
+
+    @property
+    def vars_path(self):
+        """Return fullpath string to `.ci3/vars` folder."""
+        return os.path.join(self.dotci3_path, 'vars')
+
+    @property
+    def git_branch(self):
+        """Lookup and return currently checkout git branch."""
+
+    def load_vars(self):
+        """Load vars from `.ci3` project folder."""
+        global_vars_path = os.path.join(self.vars_path, 'global.yaml')
+        if not os.path.exists(global_vars_path):
+            raise IOError('Path does not exists: %s' % global_vars_path)
+        with open(global_vars_path) as vars_stream:
+            self.config = yaml.load(vars_stream)
 
 
 class StatusCommand(CliCommand, DotCi3Mixin):
@@ -61,3 +79,4 @@ class InitCommand(CliCommand, DotCi3Mixin):
         os.makedirs(self.secrets_path)
         with open(os.path.join(self.secrets_path, '.gitignore'), 'w+') as gi:
             gi.write('# Place your keys and secrets here, but never commit')
+        os.makedirs(self.vars_path)
